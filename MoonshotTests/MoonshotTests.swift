9803546e8f22142158b52891f95bc86bfa22a387
Jazz Siddiqui <alisidd@icloud.com>
2025-11-26 00:53:45 -0800

    Initial Commit

---
+++ MoonshotTests/MoonshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotTests.swift
+//  MoonshotTests
+//
+//  Created by Jazz Siddiqui on 2025-11-26.
+//
+
+import Testing
+@testable import Moonshot
+
+struct MoonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
